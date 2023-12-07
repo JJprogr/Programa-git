3cf9b26b2deceacc0d99c36f18dd941e5544ce23
jjoviedo77@misena.edu.co <jjoviedo77@misena.edu.co>
2023-12-07 09:01:54 -0500

    Primer commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+		<FOOTER>
+			<P>Este es el pie de página</P>
+			<DIV id="cajaEnlacesFooter">
+				<NAV>
+					<UL>
+						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
+						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
+					</UL>
+				</NAV>
+
+				<NAV>
+					<UL>
+						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
+						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
+					</UL>
+				</NAV>
+
+				<NAV>
+					<UL>
+						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
+						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
+					</UL>
+				</NAV>
+			</DIV>
+			<P>Sitio creado por <A href="mailto:analisisydesarrollo@gmail.com" title="Enviar correo a Fryntiz">analisisydesarrollo@gmail.com</A>, puedes visitar mi web <A href="http://www.analisisydesarrollo.com" title="Web del programador analisisydesarrollo" target="_blank">www.analisisydesarrollo.com</A></P>
+			<P><SMALL>Licencia GPLv3 comparte y reconoce al autor | 2023 Creado y diseñado originalmente por John Oviedo</SMALL></P>
+
+			<A id="subir" href="#titulo" rel="nofollow" title="Subir"><DIV>Subir</DIV></A>
+		</FOOTER>
